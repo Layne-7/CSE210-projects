526f1d5547a271189f0492e2aec9f714f40d22fb
Derek Oliveira <derekpolicarpo@gmail.com>
2026-03-09 23:39:13 -0300

    Completed week 1 exercises.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string userAnswer = Console.ReadLine();
+        int percentage = int.Parse(userAnswer);
+
+        string letterGrade = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("You did not make it...Good luck next time, you'll make it!");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Generating a random number using Random;
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 100);
+
+        int userGuess = -1;
+
+        while (userGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
