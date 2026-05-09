9be2c241b0a2d088e5f5ecf6e002b112421c270b
Derek Oliveira <derekpolicarpo@gmail.com>
2026-05-09 15:52:58 -0300

    Added explanation comments for better understanding of functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,32 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    // We should use static when creating our functions before we start using classes...
+    static void Main(string[] args) // This is basically the main function
     {
-        DisplayWelcomeMessage();
+        DisplayWelcomeMessage(); // Calling the function
 
-        string userName = PromptUserName();
-        int userNumber = PromptUserNumber();
+        string userName = PromptUserName(); // storing the name given on the function in a variable
+        int userNumber = PromptUserNumber(); // storing the number given on the function in a variable
 
-        int squaredNumber = SquareNumber(userNumber);
+        int squaredNumber = SquareNumber(userNumber); // storing the squared number in a variable
 
-        DisplayResult(userName, squaredNumber);
+        DisplayResult(userName, squaredNumber); // display the result calling the function
     }
 
-    static void DisplayWelcomeMessage()
+    // Void means the function returns nothing - it just does something and finishes.
+    static void DisplayWelcomeMessage() // creating the function. Note: no need to add colon.
     {
-        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine("Welcome to the program!"); // printing the welcome message
     }
 
-    static string PromptUserName()
+    static string PromptUserName() // functions need to have their data type defined as well
     {
-        Console.Write("Please enter your name: ");
-        string name = Console.ReadLine();
+        Console.Write("Please enter your name: "); // getting the users name
+        string name = Console.ReadLine(); // reading the user name and storing it to a variable
 
-        return name;
+        return name; // returning the result of the function
     }
 
-    static int PromptUserNumber()
+    static int PromptUserNumber() // This function data type is int
     {
         Console.Write("Please enter your favorite number: ");
         int number = int.Parse(Console.ReadLine());
@@ -37,12 +39,13 @@ class Program
 
     static int SquareNumber(int number)
     {
-        int square = number * number;
-        return square;
+        int square = number * number; // squaring the number by multiplying
+        return square; // returing the result of multiplication, which is a squared number
     }
 
-    static void DisplayResult(string name, int square)
-    {
-        Console.WriteLine($"{name}, the square of your number is {square}");
+    static void DisplayResult(string name, int square) // void function
+    {   
+        // Displaying the message
+        Console.WriteLine($"{name}, the square of your number is {square}"); 
     }
 }
