e8d0534d60c61b44438e38438b885d85c6e6ee38
Derek Oliveira <derekpolicarpo@gmail.com>
2026-05-09 15:01:47 -0300

    Added comments explaining content

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,14 +5,16 @@ class Program
     static void Main(string[] args)
     {
         // Generating a random number using Random;
-        Random randomNumber = new Random();
-        int magicNumber = randomNumber.Next(1, 100);
+        Random randomNumber = new Random(); // using the class Random 
+        int magicNumber = randomNumber.Next(1, 100); // Getting the next integer 
 
-        int userGuess = -1;
+        int userGuess = -1; // setting up a variable before so we can always start the loop.
 
         while (userGuess != magicNumber)
-        {
+        {   // Console.Write prints the message
             Console.Write("What is your guess? ");
+            // Console.ReadLine() reads the user message on the terminal
+            //int.Parse turns the string into an integer number in order to compare guesses.
             userGuess = int.Parse(Console.ReadLine());
 
             if (magicNumber > userGuess)
