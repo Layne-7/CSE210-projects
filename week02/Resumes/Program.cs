c038c9342b8019818aedf3cac5e964720cbfc997
Derek Oliveira <derekpolicarpo@gmail.com>
2026-05-14 22:29:06 -0300

    Rewrote code and added detailed notes

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -12,13 +12,13 @@ class Program
         job1._startYear = 2019; // Variable that shows the start year
         job1._endYear = 2022; // Variable that shows the end year
 
-        Job job2 = new Job(); // From my class Job created on the other file and made public, im creating a second object
+        Job job2 = new Job(); // From my "class" Job created on the other file and made public, im creating a second object
         job2._jobTitle = "Manager";
         job2._company = "Apple";
         job2._startYear = 2022;
         job2._endYear = 2023;
 
-        Resume myResume = new Resume(); // From my class Resume created on the other file and made public, created an object
+        Resume myResume = new Resume(); // From my class "Resume" created on the other file and made public, created an object
         myResume._name = "Allison Rose"; // The class only has one public variable called _name
 
         // I created a list on the resume file and also made a foor loop in order for me to add jobs into the list
